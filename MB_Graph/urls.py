d17c571a1cafb7ce335160e4ab643d9794560b38
NicholasV-MB <73897023+NicholasV-MB@users.noreply.github.com>
2021-03-04 14:32:54 +0100

    connection to app

--- MB_Graph/urls.py
+++ MB_Graph/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from MB_Calendar import views
 
 urlpatterns = [
+    path('', include('MB_Calendar.urls')),
     path('admin/', admin.site.urls),
 ]
